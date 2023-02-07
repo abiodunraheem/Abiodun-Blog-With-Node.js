78c693e8a3a1607a23151553018dd855032e8d71
abiodunraheem <raheemabiodun23@gmail.com>
2023-02-07 16:51:10 +0100

    Add title and partials to pages

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ app.set('view engine', 'ejs')
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {title: 'Home'})
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {title: 'About'})
 })
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create')
+    res.render('create', {title: 'Create Blog'})
 })
 
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {title: '404'})
 })
